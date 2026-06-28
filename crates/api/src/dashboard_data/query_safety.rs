d23fae653066795bdcdfff15509fbee01940efd5
David Culbreth <culbreth.david@heb.com>
2026-06-27 20:44:37 -0500

    clippy fix

--- crates/api/src/dashboard_data/query_safety.rs
+++ crates/api/src/dashboard_data/query_safety.rs
@@ -29,6 +29,12 @@ pub struct SafeQueryBindings {
     values: BTreeMap<String, TypedBindValue>,
 }
 
+impl Default for SafeQueryBindings {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl SafeQueryBindings {
     pub fn new() -> Self {
         Self {
